faadd77fb39d883b62178d6cd21b941392ada50c
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-26 10:09:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener(DOMContentLoaded, function() {
+	let element = document.getElementById("level");
+
+	let currElement = element;
+	let count = 0;
+
+	while(currElement){
+		count++;
+		currElement = currElement.parentElement;
+	}
+
+	alert(`The level of the element is: ${count}`);
+})
